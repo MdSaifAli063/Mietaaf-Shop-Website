0877c5427b018a22f088aebe6430596bb125a9b2
MdSaifAli063 <mdsaifali6303@gmail.com>
2026-08-04 01:44:27 +0530

    added

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -13,9 +13,16 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#171512",
     icons: [
       {
-        src: "/favicon.ico",
-        sizes: "any",
-        type: "image/x-icon",
+        src: "/icon.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/branding/mietaaf-icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any",
       },
     ],
   };
